daca9b93b907e47484c4e420c4349257b24b8513
Luke Childs <lukechilds123@gmail.com>
2017-10-07 18:32:44 +0100

    Test reverseShell returns shell code with /host:port variables

--- test/test.js
+++ test/test.js
@@ -4,3 +4,11 @@ import reverseShell from 'this';
 test('reverseShell is a function', t => {
 	t.is(typeof reverseShell, 'function');
 });
+
+test('reverseShell returns shell code with /host:port variables', t => {
+	const req = {
+		url: '/foo:bar'
+	};
+	const returnValue = reverseShell(req);
+	t.true(returnValue.indexOf('("foo",bar)') > -1);
+});
